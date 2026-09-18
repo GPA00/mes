a86ea64a44007812812e36277ec743aa2ad2cdff
Administrator <1228973684@qq.com>
2026-09-18 17:32:04 +0800

    接入uniapp移动端

---
+++ yudao-ui/yudao-ui-admin-uniapp/src/utils/constants.ts
@@ -0,0 +1,9 @@
+export * from './constants/biz-ai-enum'
+export * from './constants/biz-bpm-enum'
+export * from './constants/biz-infra-enum'
+export * from './constants/biz-mes-enum'
+export * from './constants/biz-mp-enum'
+export * from './constants/biz-system-enum'
+export * from './constants/biz-wms-enum'
+export * from './constants/dict-enum'
+
